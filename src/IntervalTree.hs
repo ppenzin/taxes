254ae6325071a0acee7e1d0ae7284f19e3d66dfd
Peter Penzin <penzin.dev@gmail.com>
2014-04-07 21:40:08 -0700

    Moved Interval Tree to a separate package

--- src/IntervalTree.hs
+++
@@ -1,8 +0,0 @@
-module IntervalTree where
-
-data Bound a = Include a | Exclude a
-data Interval a = AnInterval {high :: Bound a, low :: Bound a}
-
-data IntervalTree a b = ILeaf {value :: b, interval ::Interval a}
-   | IBranch { value :: b, left :: IntervalTree a b, right:: IntervalTree a b}
-
