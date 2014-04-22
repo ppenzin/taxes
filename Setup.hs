d76d5e3800cd47ddbc167127a32aeb237aaab8a6
Peter Penzin <penzin.dev@gmail.com>
2014-04-21 18:09:46 -0700

    Added a dummy (and always failing) test

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,8 @@
 import Distribution.Simple
-main = defaultMain
+
+main = defaultMainWithHooks hooks
+  where hooks = simpleUserHooks { runTests = runTests' }
+
+runTests' :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
+runTests' _ _ _ lbi = system testprog >> return ()
+  where testprog = (buildDir lbi) </> "test" </> "test"
