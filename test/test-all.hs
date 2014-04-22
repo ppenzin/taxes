8def1a4e600eb696f3957146e987922f1832518f
Peter Penzin <penzin.dev@gmail.com>
2014-04-22 03:23:17 -0700

    Added a single test under quickcheck.

--- test/test-all.hs
+++ test/test-all.hs
@@ -1,7 +1,12 @@
 module Main where
 
-import System.Exit (exitFailure)
+import Control.Monad
+import Test.QuickCheck
+import System.Exit
 
+main :: IO ()
 main = do
-    putStrLn "This test always fails!"
-    exitFailure
+  result <- quickCheckResult prop
+  unless (isSuccess result) exitFailure
+
+prop x = True
